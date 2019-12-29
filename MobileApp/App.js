108e8b362e794a051a0ec2c2156309af22158911
David <dhahn123@gmail.com>
2019-12-29 10:13:51 -0800

    connect socketio client with server

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import io from "socket.io-client";
 
 export default function App() {
+// empty can be used to pass in variables
+// useEffect will rerun based on variables otherwise empty array will only run one time
+  useEffect(function() {
+    io("http://192.168.0.10:3001")
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,7 +1,7 @@
 const io = require("socket.io")();
 
 io.on("connection", function(){
-    console.log("a User connected!")
+    console.log("User connected!")
 })
 
 io.listen(3001);
