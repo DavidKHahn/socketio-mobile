0d3d050ac3af7c6c5607222ee3812c238ae611cf
David <dhahn123@gmail.com>
2019-12-30 13:52:52 -0800

    userId Map correctly from backend, cleanup HomeScreen

--- MobileApp/screens/HomeScreen.js
+++ MobileApp/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 console.ignoredYellowBox = ['Remote debugger'];
 import React, { useEffect, useRef, useState } from 'react';
-import { KeyboardAvoidingView, Platform, StyleSheet, View, YellowBox } from 'react-native';
+import { KeyboardAvoidingView, Platform, View, YellowBox } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 import io from "socket.io-client";
 YellowBox.ignoreWarnings([
@@ -18,42 +18,9 @@ const socket = useRef(null);
   useEffect(() => {
     socket.current = io("http://192.168.1.3:3001");
     socket.current.on("message", message => {
-        const testMessage = {
-            _id: 3,
-            text: 'Hello developer',
-            createdAt: new Date(),
-            user: {
-              _id: 2,
-              name: 'React Native',
-              avatar: 'https://placeimg.com/140/140/any'
-            }
-          };
-        testMessage.text = message;
         // saves sent msgs
-        setRecvMessages(prevState => GiftedChat.append(prevState, testMessage));
+        setRecvMessages(prevState => GiftedChat.append(prevState, message));
     });
-    setRecvMessages([
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any'
-          }
-        },
-        {
-            _id: 2,
-            text: 'Hello from myself!',
-            createdAt: new Date(),
-            user: {
-              _id: 1,
-              name: 'React Native',
-              avatar: 'https://placeimg.com/140/140/any'
-            }
-          },
-      ]);
   }, []);
 
   const onSend = (messages) => {
@@ -76,12 +43,3 @@ const socket = useRef(null);
   </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,5 +1,16 @@
 const io = require("socket.io")();
 
+// const testMessage = {
+//     _id: 3,
+//     text: 'Hello developer',
+//     createdAt: new Date(),
+//     user: {
+//       _id: 2,
+//       name: 'React Native',
+//       avatar: 'https://placeimg.com/140/140/any'
+//     }
+//   };
+
 io.on("connection", socket => {
     console.log("User connected!");
     // listen for msg event
