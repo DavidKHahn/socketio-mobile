f458d97ebc3418bf98ddc503baff9a8ff5018f7c
David <dhahn123@gmail.com>
2019-12-29 16:48:41 -0800

    refresh message box after msg is sent

--- MobileApp/screens/HomeScreen.js
+++ MobileApp/screens/HomeScreen.js
@@ -1,29 +1,37 @@
 console.ignoredYellowBox = ['Remote debugger'];
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { StyleSheet, Text, TextInput, View, YellowBox } from 'react-native';
 import io from "socket.io-client";
 YellowBox.ignoreWarnings([
     'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
 ]);
 
-
 export default function HomeScreen() {
 // messageToSend current state
 // setMessageToSend updating state
 const [messageToSend, setMessageToSend] = useState("");
+const socket = useRef(null);
 // empty can be used to pass in variables
 // useEffect will rerun based on variables otherwise empty array will only run one time
-  useEffect(function() {
-    io("http://192.168.1.2:3001")
+  useEffect(() => {
+    socket.current = io("http://192.168.1.2:3001");
   }, [])
+
+  const sendMessage = () => {
+    socket.current.emit("message", messageToSend);
+    // clears message after sent
+    setMessageToSend("");
+  }
 
   return (
     <View style={styles.container}>
       <Text>Hello, Welcome to MobileApp!</Text>
       <TextInput
         value={messageToSend}
-        onChangeText={(text) => setMessageToSend(text)} // updates state of message
-        placeholder="Enter chat message..." />
+        onChangeText={text => setMessageToSend(text)} // updates state of message
+        placeholder="Enter chat message..."
+        onSubmitEditing={sendMessage}
+        />
     </View>
   );
 }
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,7 +1,10 @@
 const io = require("socket.io")();
 
-io.on("connection", function(){
-    console.log("User connected!")
+io.on("connection", socket => {
+    console.log("User connected!");
+    socket.on("message", message => {
+        console.log(message);
+    })
 })
 
 io.listen(3001);
