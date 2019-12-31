658451322653f583829a74313dde5f431d60f56f
David <dhahn123@gmail.com>
2019-12-30 17:50:36 -0800

    separate files in server.js into message.handler.js

--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,34 +1,15 @@
 const io = require("socket.io")();
+const messageHandler = require("./handlers/message.handler");
 
 let currentUserId = 2;
-let currentMessageId = 1;
 const userIds = {};
-
-function createMessage(userId, messageText) {
-    return {
-    _id: currentMessageId++,
-    text: messageText,
-    createdAt: new Date(),
-    user: {
-      _id: userId,
-      name: 'Test User',
-      avatar: 'https://placeimg.com/140/140/any'
-    }
-  };
-}
 
 io.on("connection", socket => {
     console.log("User connected!");
     console.log(socket.id);
     userIds[socket.id] = currentUserId++;
     // listen for msg event
-    socket.on("message", messageText => {
-        console.log(messageText);
-        const userId = userIds[socket.id];
-        const message = createMessage(userId, messageText);
-        console.log(message);
-        socket.broadcast.emit("mesage", message);
-    })
+    messageHandler.handleMessage(socket, userIds)
 })
 
 io.listen(3001);
