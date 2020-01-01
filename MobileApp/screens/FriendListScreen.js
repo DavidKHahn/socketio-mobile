9bf8cf7646997322989da53f75951a6980e372c9
David <dhahn123@gmail.com>
2019-12-31 21:56:55 -0800

    yarn add uuid, display users online and disconnecting users event

--- MobileApp/screens/FriendListScreen.js
+++ MobileApp/screens/FriendListScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { useSelector } from 'react-redux';
 
 export default function FriendListScreen(){
+    const usersOnline = useSelector(state => state.usersOnline);
+    console.log("usersOnline", usersOnline);
+
     return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>FriendList!</Text>
+        <FlatList
+            data={usersOnline}
+            renderItem={({item}) => {
+                console.log("item", item);
+                return <Text>{item.username}</Text>;
+            }}
+            keyExtractor={item => item.username}
+        />
     </View>
     )
 }
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,26 +1,35 @@
 const io = require("socket.io")();
 const messageHandler = require("./handlers/message.handler");
+const uuidv1 = require("uuid/v1");
 
-let currentUserId = 2;
-const userIds = {};
+const users = {};
 
 function createUserAvatarUrl(){
     const rand1 = Math.round(Math.random() * 200 + 100);
     const rand2 = Math.round(Math.random() * 200 + 100);
     return `https://placeimg.com/${rand1}/${rand2}/any`;
+}
 
+function createUsersOnline() {
+    const values = Object.values(users);
+    const onlyWithUsernames = values.filter(u => u.username !== undefined);
+    return onlyWithUsernames;
 }
 
 io.on("connection", socket => {
     console.log("User connected!");
     console.log(socket.id);
-    users[socket.id] = {userId: currentUserId++}
+    users[socket.id] = {userId: uuidv1()}
     socket.on("join", username => {
       users[socket.id].username = username
       // creates unique user avatar
       users[socket.id].avatar = createUserAvatarUrl();
         // listen for msg event
       messageHandler.handleMessage(socket, users)
+    })
+    socket.on("disconnect", () => {
+        delete users[socket.id];
+        io.emit("action", { type: "users_online", data: createUsersOnline() })
     })
     socket.on("action", action => {
         switch(action.type) {
@@ -33,10 +42,8 @@ io.on("connection", socket => {
                 console.log("Got join event", action.data);
                 users[socket.id].username = action.data;
                 users[socket.id].avatar = createUserAvatarUrl();
-                const values = Object.values(users);
-                const onlyWithUsernames = values.filter(u => u.username !== undefined);
                 // io emits to all users while socket to only same user
-                io.emit("action", {type: "users_online", data: onlyWithUsernames});
+                io.emit("action", {type: "users_online", data: createUsersOnline()});
                 break;
         }
     })
