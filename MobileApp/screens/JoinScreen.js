0cf651c68267f6aadcd9b65c0562e2cb9f010e2f
David <dhahn123@gmail.com>
2019-12-31 17:44:46 -0800

    set up chat img

--- MobileApp/screens/JoinScreen.js
+++ MobileApp/screens/JoinScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, TextInput, View } from 'react-native';
 
 export default function JoinScreen() {
     return (
-        <View>
+        <View style={{flex: 1, alignContent: "center", justifyContent: "center"}}>
+            <Image source={require("../assets/chat-icon.png")} />
+            <TextInput placeholder="Enter username" />
+            <Button title="Join Chat" />
             <Text>JoinScreen</Text>
         </View>
     )
