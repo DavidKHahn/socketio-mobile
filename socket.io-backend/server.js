6bc8a34ca9ebe72ca9926615834fd57edadef529
David <dhahn123@gmail.com>
2019-12-31 18:59:15 -0800

    create new avatars for each user

--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -4,15 +4,24 @@ const messageHandler = require("./handlers/message.handler");
 let currentUserId = 2;
 const userIds = {};
 
+function createUserAvatarUrl(){
+    const rand1 = Math.round(Math.random() * 200 + 100);
+    const rand2 = Math.round(Math.random() * 200 + 100);
+    return `https://placeimg.com/${rand1}/${rand2}/any`;
+
+}
+
 io.on("connection", socket => {
     console.log("User connected!");
     console.log(socket.id);
-    userIds[socket.id] = {userId: currentUserId++}
+    users[socket.id] = {userId: currentUserId++}
     socket.on("join", username => {
-      userIds[socket.id].username = username
+        users[socket.id].username = username
+      // creates unique user avatar
+      users[socket.id].avatar = createUserAvatarUrl();
     })
     // listen for msg event
-    messageHandler.handleMessage(socket, userIds)
+    messageHandler.handleMessage(socket, users)
 })
 
 io.listen(3001);
