b71f9e929cbd6add6b0c1d66821c3589889b9320
David <dhahn123@gmail.com>
2019-12-31 17:53:34 -0800

    center username text input, space around logo and button, change fontsize

--- MobileApp/screens/JoinScreen.js
+++ MobileApp/screens/JoinScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Image, KeyboardAvoidingView, Platform, TextInput, View } from 'react-native';
+import { Button, Image, KeyboardAvoidingView, Platform, TextInput, View } from 'react-native';
 
 export default function JoinScreen() {
     return (
         <View style={{flex: 1, alignContent: "center", justifyContent: "center"}}>
             <Image resizeMode="contain" style={{ flex: 1 }} source={require("../assets/chat-icon.png")} />
-            <View style={{ flex: 1 }}>
-            <TextInput placeholder="Enter username" />
+            <View style={{ flex: 1, justifyContent: "space-around" }}>
+            <TextInput style={{ fontSize: 30, textAlign: "center" }}placeholder="Enter username" />
             <Button title="Join Chat" />
             </View>
             {Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />}
