c05f74cf9630ca48e1bcd70dcba8755035e68068
David <dhahn123@gmail.com>
2019-12-31 17:47:28 -0800

    fix resize of img when typing input

--- MobileApp/screens/JoinScreen.js
+++ MobileApp/screens/JoinScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Image, Text, TextInput, View } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, TextInput, View } from 'react-native';
 
 export default function JoinScreen() {
     return (
         <View style={{flex: 1, alignContent: "center", justifyContent: "center"}}>
-            <Image source={require("../assets/chat-icon.png")} />
+            <Image resizeMode="contain" style={{ flex: 1 }} source={require("../assets/chat-icon.png")} />
+            <View style={{ flex: 1 }}>
             <TextInput placeholder="Enter username" />
             <Button title="Join Chat" />
-            <Text>JoinScreen</Text>
+            </View>
+            {Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />}
         </View>
     )
 }
