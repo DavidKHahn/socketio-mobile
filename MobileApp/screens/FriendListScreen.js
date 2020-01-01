0e850b409875a1040ef06156576f233fcacead79
David <dhahn123@gmail.com>
2019-12-31 22:23:27 -0800

    adding user avatar, stylesheet for friendlist

--- MobileApp/screens/FriendListScreen.js
+++ MobileApp/screens/FriendListScreen.js
@@ -1,22 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 
 export default function FriendListScreen(){
     const usersOnline = useSelector(state => state.usersOnline);
     console.log("usersOnline", usersOnline);
 
+    const { itemContainerStyle } = styles;
+
     return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{ flex: 1 }}>
         <FlatList
             data={usersOnline}
             renderItem={({item}) => {
                 console.log("item", item);
-                return <Text>{item.username}</Text>;
+                return (
+                <View style={itemContainerStyle}>
+                    <Image style={{ width: 100, height: 100, borderRadius: 50 }} source={{ uri: item.avatar }} />
+                    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                    <Text>{item.username}</Text>
+                </View>
+                </View>
+                );
             }}
-            keyExtractor={item => item.username}
+            keyExtractor={item => item.userId}
         />
     </View>
     )
 }
+
+const styles = StyleSheet.create({
+    itemContainerStyle: {flex: 1, flexDirection: "row"}
+})
